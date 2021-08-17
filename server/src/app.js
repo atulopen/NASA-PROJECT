43d8bcc773855fe2456c7f883b96b03b271b88f8
Arthur Atul <arthur.atul@ithands.biz>
2021-08-17 15:34:13 +0530

    add cors middleware

--- server/src/app.js
+++ server/src/app.js
@@ -1,9 +1,14 @@
 const express = require('express');
+const cors = require('cors');
 const planetsRouter = require('./routes/planets/planets.router');
 
 const app = express();
 
 app.use(express.json());
+
+app.use(cors({
+    origin: 'localhost:3000'
+}));
 
 app.use('/planets', planetsRouter);
 
