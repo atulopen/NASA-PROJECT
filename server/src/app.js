978f073bd65b03456a7a2605edb892827786805b
Arthur Atul <arthur.atul@ithands.biz>
2021-08-19 14:21:46 +0530

    add deploy command to create react build and server from the server

--- server/src/app.js
+++ server/src/app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const planetsRouter = require('./routes/planets/planets.router');
+const path = require('path');
 
 const app = express();
 
@@ -10,6 +11,12 @@ app.use(cors({
     origin: 'http://localhost:3000'
 }));
 
+app.use(express.static(path.join(__dirname, '..', 'public')))
+
 app.use('/planets', planetsRouter);
+
+app.get('/', (req, res) => {
+    return res.sendFile(path.join(__dirname, '..', 'public', 'index.html'));
+})
 
 module.exports = app;
