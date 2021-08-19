f105c8613ba57a2187d5a880a2d9479e644156ae
Arthur Atul <arthur.atul@ithands.biz>
2021-08-19 14:43:00 +0530

    added morgan middleware for logging

--- server/src/app.js
+++ server/src/app.js
@@ -2,10 +2,14 @@ const express = require('express');
 const cors = require('cors');
 const planetsRouter = require('./routes/planets/planets.router');
 const path = require('path');
+const morgan = require('morgan');
 
 const app = express();
 
 app.use(express.json());
+
+app.use(morgan('combined'));
+
 
 app.use(cors({
     origin: 'http://localhost:3000'
@@ -14,6 +18,7 @@ app.use(cors({
 app.use(express.static(path.join(__dirname, '..', 'public')))
 
 app.use('/planets', planetsRouter);
+
 
 app.get('/', (req, res) => {
     return res.sendFile(path.join(__dirname, '..', 'public', 'index.html'));
