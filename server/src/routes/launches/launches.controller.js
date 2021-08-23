e1412bd3bc53d140839845b49d1e6d808d1ef5ed
Arthur Atul <arthur.atul@ithands.biz>
2021-08-23 16:56:08 +0530

    add validations

--- server/src/routes/launches/launches.controller.js
+++ server/src/routes/launches/launches.controller.js
@@ -6,7 +6,20 @@ function httpGetAllLaunches(req, res) {
 
 function httpAddNewLaunch(req, res) {
     const launch = req.body;
+    if (!launch.mission || !launch.rocket || !launch.launchDate || !launch.target) {
+        res.status(400);
+        return res.json({
+            error: 'Missing Required fields'
+        })
+    }
     launch.launchDate = new Date(launch.launchDate);
+
+    if (isNaN(launch.launchDate)) {
+        res.status(400);
+        return res.json({
+            error: "Invalid launch date"
+        })
+    }
     addNewLaunch(launch);
 
     return res.status(201).json(launch);
