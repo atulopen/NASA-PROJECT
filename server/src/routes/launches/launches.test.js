fdeb86fd046f462fbf4490d9dae175688725c195
Arthur Atul <arthur.atul@ithands.biz>
2021-08-25 22:09:30 +0530

    extend unit test to matches the response body

--- server/src/routes/launches/launches.test.js
+++ server/src/routes/launches/launches.test.js
@@ -2,18 +2,36 @@ const request = require('supertest');
 const app = require('../../app');
 
 describe('Test GET /launches', () => {
-    test('It should respond with 200 success', () => {
-        const response = 200;
-        expect(response).toBe(200);
+    test('It should respond with 200 success', async () => {
+        await request(app)
+            .get('/launches')
+            .expect('Content-Type', /json/)
+            .expect(200);
     })
 })
 
 describe('Test POST /launches', () => {
-    test('It should respond with 200 success', async () => {
+    const launchDataWithoutDate = {
+        mission: 'Arthur Mission',
+        rocket: 'Explorer IS1',
+        target: 'Kepler-1652 b',
+    }
+
+    const completeLaunchData = {
+        ...launchDataWithoutDate,
+        launchDate: new Date('December 22, 2030'),
+    }
+    test('It should respond with 201 created', async () => {
         const response = await request(app)
-            .get('/launches')
+            .post('/launches')
+            .send(completeLaunchData)
             .expect('Content-Type', /json/)
-            .expect(200);
+            .expect(201);
+
+        const launchDate = new Date(completeLaunchData.launchDate).valueOf();
+        const responseLaunchDate = new Date(response.body.launchDate).valueOf();
+        expect(launchDate).toBe(responseLaunchDate);
+        expect(response.body).toMatchObject(launchDataWithoutDate)
     })
 
     test('It should catch missing required properties', () => {
