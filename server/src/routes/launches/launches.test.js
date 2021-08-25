98e21e170dbae527318741857d720a1dd8687e4b
Arthur Atul <arthur.atul@ithands.biz>
2021-08-25 21:45:10 +0530

    test api end points with supertest

--- server/src/routes/launches/launches.test.js
+++ server/src/routes/launches/launches.test.js
@@ -1,3 +1,6 @@
+const request = require('supertest');
+const app = require('../../app');
+
 describe('Test GET /launches', () => {
     test('It should respond with 200 success', () => {
         const response = 200;
@@ -6,8 +9,11 @@ describe('Test GET /launches', () => {
 })
 
 describe('Test POST /launches', () => {
-    test('It should respond with 200 success', () => {
-
+    test('It should respond with 200 success', async () => {
+        const response = await request(app)
+            .get('/launches')
+            .expect('Content-Type', /json/)
+            .expect(200);
     })
 
     test('It should catch missing required properties', () => {
